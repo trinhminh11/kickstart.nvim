7af1af27932541c18509b7fc99d63ec55f536209
trinhminh11 <trinhtheminh1104@gmail.com>
2026-09-02 17:58:30 +0700

    update prefix string

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -5,21 +5,11 @@
   (#match? @function.dunder "^__.*__$")
   (#set! priority 130))
 
-; 1-char prefixes: f r u b
 ((string_start) @string.prefix
-  (#match? @string.prefix "^[fFrRuUbB]['\"]$")
+  (#match? @string.prefix "^[^'\"]+['\"]$")
   (#offset! @string.prefix 0 0 0 -1))
 
 ((string_start) @string.prefix
-  (#match? @string.prefix "^[fFrRuUbB]['\"]{3}$")
-  (#offset! @string.prefix 0 0 0 -3))
-
-; 2-char prefixes: fr rf br rb
-((string_start) @string.prefix
-  (#match? @string.prefix "^([fF][rR]|[rR][fF]|[bB][rR]|[rR][bB])['\"]$")
-  (#offset! @string.prefix 0 0 0 -1))
-
-((string_start) @string.prefix
-  (#match? @string.prefix "^([fF][rR]|[rR][fF]|[bB][rR]|[rR][bB])['\"]{3}$")
+  (#match? @string.prefix "^[^'\"]+['\"]{3}$")
   (#offset! @string.prefix 0 0 0 -3))
 
